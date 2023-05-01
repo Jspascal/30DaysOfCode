ff8e3d6ebfa4eecbcd56f0f11558ddeda1bdf2fa
Joseph NOMO <fulgaros@gmail.com>
2023-05-01 01:04:17 +0100

    implementing day8 in php

---
+++ Day8/day8.php
@@ -0,0 +1,18 @@
+<?php
+
+$T = (int)readline();
+
+$contacts = array();
+
+for ($i = 0; $i < $T; $i++) {
+  $contact = explode(" ", readline(), 2);
+  $contacts[$contact[0]] = $contact[1];
+}
+
+for ($i = 0; $i < $T; $i++) {
+  $contact_name = readline();
+  if (array_key_exists($contact_name, $contacts))
+    echo $contact_name . "=" . $contacts[$contact_name] . PHP_EOL;
+  else
+    echo "Not found" . PHP_EOL;
+}
